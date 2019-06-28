919bdf7f2f8d210a7440077a2645476111c9da8f
co4524 <monster3697@gmail.com>
2019-06-29 02:30:35 +0800

    6/28 update

--- remoteConfig/changeIp.sh
+++ remoteConfig/changeIp.sh
@@ -1,11 +1,16 @@
 path=$HOME/mytestnet/node
+URL_path=$HOME/evm-lite-js/test/baseURL
 nodeNum=$1
 base=$2
+rm $URL_path
 for ((i=0 ; i<$1 ; i++)) {
 	ip=$3
-	for ((j=0 ; j<$1 ; j++)){
-		#echo $base.$ip
+	for ((j=$1-1 ; j>-1 ; j--)){
+		if [ "$i" = "0" ]
+		then
+			echo "http://$base.$ip:8080" >> $URL_path
+		fi
 		sed -i "s/node$j/$base.$ip/g" $path$i/tendermint/config/config.toml
-		let ip=ip+1	
+		let ip=ip-1	
 	}
 }
